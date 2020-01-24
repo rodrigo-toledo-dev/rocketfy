214905952da144f8d23b0ae939a1fc6eebb83a25
RToledo <rodrigo@rtoledo.inf.br>
2020-01-24 17:45:33 -0300

    header board and initial lists

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Header = () => {
+  return (
+  <Container>
+    Rocketfy
+  </Container>
+)
+};
+
+
+export default Header;
---
+++ src/components/List/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const List = () => {
+  return (
+    <Container>
+      <header>
+        <h2>Tarefas</h2>
+        <button type="button">
+
+        </button>
+      </header>
+    </Container>
+
+  )
+};
+
+export default List;
